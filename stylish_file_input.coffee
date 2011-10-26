942fab28de091d60233a68b3c86943b28242edb3
Mo Wang <mo.hy.wang@gmail.com>
2011-10-25 20:33:19 -0700

    add

--- stylish_file_input.coffee
+++ stylish_file_input.coffee
@@ -3,19 +3,50 @@ class StylishFileInput
     constructor: (htmlFileInputElement, text = "Choose File") ->
         @_input = htmlFileInputElement
         @_text = text
+
+        @_label = null
         
         _buildUI.call this
+        # _registerEventHandlers.call this
 
     _buildUI = ->
         console.log "build UI"
+        
         parentNode = @_input.parentNode
         label = document.createElement "label"
+        label.className = "stylish-file-input"
         inputSibling = @_input.nextSibling
         parentNode.insertBefore label, inputSibling
         label.appendChild @_input
         textSpan = document.createElement "span"
         textSpan.innerHTML = @_text
-        label.appendChild textSpan
+        label.insertBefore textSpan, @_input
+
+        @_label = label
+
+        _hideInput.call this
+        
+    _hideInput = ->
+        label = @_label
+        label.style.position = "relative"
+        label.style.display = "inline-block"
+        label.style.overflow = "hidden"
+
+        input = @_input
+        input.style.position = "absolute"
+        input.style.right = "0px"
+        input.style.top = "0px"
+        input.style.opacity = 0
+        input.style.cursor = "pointer"
+        input.style.fontSize = "500px"
+    
+    _registerEventHandlers = ->
+        $label = $(@_label)
+        $label.hover ((e)->
+            $label.addClass "state-hover"
+        ), ((e)->
+            $label.removeClass "state-hover"
+        )
 
 window.StylishFileInput = StylishFileInput
   
--- stylish_file_input.js
+++ stylish_file_input.js
@@ -1,13 +1,14 @@
 (function() {
   var StylishFileInput;
   StylishFileInput = (function() {
-    var _buildUI;
+    var _buildUI, _hideInput, _registerEventHandlers;
     function StylishFileInput(htmlFileInputElement, text) {
       if (text == null) {
         text = "Choose File";
       }
       this._input = htmlFileInputElement;
       this._text = text;
+      this._label = null;
       _buildUI.call(this);
     }
     _buildUI = function() {
@@ -15,12 +16,38 @@
       console.log("build UI");
       parentNode = this._input.parentNode;
       label = document.createElement("label");
+      label.className = "stylish-file-input";
       inputSibling = this._input.nextSibling;
       parentNode.insertBefore(label, inputSibling);
       label.appendChild(this._input);
       textSpan = document.createElement("span");
       textSpan.innerHTML = this._text;
-      return label.appendChild(textSpan);
+      label.insertBefore(textSpan, this._input);
+      this._label = label;
+      return _hideInput.call(this);
+    };
+    _hideInput = function() {
+      var input, label;
+      label = this._label;
+      label.style.position = "relative";
+      label.style.display = "inline-block";
+      label.style.overflow = "hidden";
+      input = this._input;
+      input.style.position = "absolute";
+      input.style.right = "0px";
+      input.style.top = "0px";
+      input.style.opacity = 0;
+      input.style.cursor = "pointer";
+      return input.style.fontSize = "500px";
+    };
+    _registerEventHandlers = function() {
+      var $label;
+      $label = $(this._label);
+      return $label.hover((function(e) {
+        return $label.addClass("state-hover");
+      }), (function(e) {
+        return $label.removeClass("state-hover");
+      }));
     };
     return StylishFileInput;
   })();
